e8eee1603c2b7fd90ebf8d732d4e48b7b151a201
Stanly0407 <Shelestova_S_V@mail.ru>
2020-11-15 00:28:58 +0300

    first

---
+++ src/main/java/stanly0407/sarafan/controller/MessageController.java
@@ -0,0 +1,60 @@
+package stanly0407.sarafan.controller;
+
+import org.springframework.web.bind.annotation.*;
+import stanly0407.sarafan.exceptions.NotFoundException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+
+    private int counter = 4;
+
+    private List<Map<String, String>> messages = new ArrayList<>(){{
+        add(new HashMap<>() {{put("id", "1"); put("text", "First message");}});
+        add(new HashMap<>() {{put("id", "2"); put("text", "Second message");}});
+        add(new HashMap<>() {{put("id", "3"); put("text", "Third message");}});
+    }};
+
+    @GetMapping
+    public List<Map<String, String>> list(){
+        return messages;
+    }
+    @GetMapping("{id}")
+    public Map<String, String> getOne(@PathVariable String id){
+        return getMessage(id); // чтоб выдать 404, если полученный id не соответсвует - для этого создаем exception
+
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
+        return messages.stream()
+                .filter(messages -> messages.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
+    }
+
+    @PostMapping
+    public Map<String, String>  create (@RequestBody Map<String, String> message){
+       message.put("id", String.valueOf(counter++)); //получаем новый message от юзера, добавляем ему id
+        messages.add(message);
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update (@PathVariable String id, @RequestBody Map<String, String> message){
+        Map<String, String> messageFromDb = getMessage(message.get("id"));
+        messageFromDb.putAll(message);
+        messageFromDb.put("id", id);
+        return messageFromDb;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(){
+        getMessage()
+    }
+
+}
---
+++ src/test/java/stanly0407/sarafan/SarafanApplicationTests.java
@@ -0,0 +1,13 @@
+package stanly0407.sarafan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarafanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
